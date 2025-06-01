b81a2eda0128f6edd64a911418975da924622a1f
xve001 <nandaanamee@gmail.com>
2025-06-01 13:39:43 +0000

    setup for building

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
-# Compile script for FSociety kernel
+# Compile script for Gilver kernel
 # Copyright (C) 2020-2021 Adithya R.
+# Copyright (C) 2021-2025 Richard
+# Copyright (C) 2025 Vergilantte
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="FSociety-surya-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="Gilver-surya-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-20"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="surya_defconfig"
@@ -51,12 +53,12 @@ sync_repo() {
 }
 
 if [[ $1 = "-u" || $1 = "--update" ]]; then
-    sync_repo $AK3_DIR "https://github.com/rd-stuffs/AnyKernel3.git" "FSociety" true
-    sync_repo $TC_DIR "https://bitbucket.org/rdxzv/clang-standalone.git" "20" true
+    sync_repo $AK3_DIR "https://github.com/xvergilantte/AnyKernel3.git" "Gilver" true
+    sync_repo $TC_DIR "https://bitbucket.org/xvergil/clang-standalone.git" "20" true
 	exit
 else
-    sync_repo $AK3_DIR "https://github.com/rd-stuffs/AnyKernel3.git" "FSociety" false
-    sync_repo $TC_DIR "https://bitbucket.org/rdxzv/clang-standalone.git" "20" false
+    sync_repo $AK3_DIR "https://github.com/xvergilantte/AnyKernel3.git" "Gilver" false
+    sync_repo $TC_DIR "https://bitbucket.org/xvergil/clang-standalone.git" "20" false
 fi
 
 if [ ! -d "$AK3_DIR" ] || [ ! -d "$TC_DIR" ]; then
@@ -80,6 +82,7 @@ fi
 
 CLEAN_BUILD=false
 ENABLE_KSU=false
+ENABLE_KSUSUSFS=false
 
 for arg in "$@"; do
 	case $arg in
@@ -88,7 +91,11 @@ for arg in "$@"; do
 			;;
 		-s|--su)
 			ENABLE_KSU=true
-			ZIPNAME="${ZIPNAME/FSociety-surya/FSociety-KSU}"
+			ZIPNAME="${ZIPNAME/Gilver-surya/Gilver-KSU}"
+			;;
+		-ss|--susus)
+			ENABLE_KSUSUSFS=true
+			ZIPNAME="${ZIPNAME/Gilver-surya/Gilver-KSUSUSFS}"
 			;;
 		*)
 			echo "Unknown argument: $arg"
@@ -103,19 +110,45 @@ if $CLEAN_BUILD; then
 fi
 
 if $ENABLE_KSU; then
-	echo "Building with KSU support..."
+	echo "Building with KSU support"
 	KSU_DEFCONFIG="ksu_${DEFCONFIG}"
 	KSU_DEFCONFIG_PATH="arch/arm64/configs/${KSU_DEFCONFIG}"
 	cp arch/arm64/configs/$DEFCONFIG $KSU_DEFCONFIG_PATH
-	sed -i 's/# CONFIG_KSU is not set/CONFIG_KSU=y/g' $KSU_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' $KSU_DEFCONFIG_PATH
 	trap '[[ -f $KSU_DEFCONFIG_PATH ]] && rm -f $KSU_DEFCONFIG_PATH' EXIT
+fi
+
+if $ENABLE_KSUSUSFS; then
+	echo "Building with KSU and SuSFS support..."
+	KSUSUSFS_DEFCONFIG="ksususfs_${DEFCONFIG}"
+	KSUSUSFS_DEFCONFIG_PATH="arch/arm64/configs/${KSUSUSFS_DEFCONFIG}"
+	cp arch/arm64/configs/$DEFCONFIG $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS=n/CONFIG_KSU_SUSFS=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=n/CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_SUS_PATH=n/CONFIG_KSU_SUSFS_SUS_PATH=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_SUS_MOUNT=n/CONFIG_KSU_SUSFS_SUS_MOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=n/CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=n/CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_SUS_KSTAT=n/CONFIG_KSU_SUSFS_SUS_KSTAT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_TRY_UMOUNT=n/CONFIG_KSU_SUSFS_TRY_UMOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=n/CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_SPOOF_UNAME=n/CONFIG_KSU_SUSFS_SPOOF_UNAME=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_ENABLE_LOG=n/CONFIG_KSU_SUSFS_ENABLE_LOG=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=n/CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=n/CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	sed -i 's/CONFIG_KSU_SUSFS_OPEN_REDIRECT=n/CONFIG_KSU_SUSFS_OPEN_REDIRECT=y/g' $KSUSUSFS_DEFCONFIG_PATH
+	trap '[[ -f $KSUSUSFS_DEFCONFIG_PATH ]] && rm -f $KSUSUSFS_DEFCONFIG_PATH' EXIT
 fi
 
 echo -e "\nStarting compilation...\n"
 if $ENABLE_KSU; then
-	make $KSU_DEFCONFIG
+		make $KSU_DEFCONFIG
+elif
+   $ENABLE_KSUSUSFS; then
+		make $KSUSUSFS_DEFCONFIG
 else
-	make $DEFCONFIG
+		make $DEFCONFIG
 fi
 make -j$(nproc --all) LLVM=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
 
@@ -128,10 +161,11 @@ if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
 	cp -r $AK3_DIR AnyKernel3
 	cp $kernel $dtb $dtbo AnyKernel3
 	cd AnyKernel3
-	git checkout FSociety &> /dev/null
+	git checkout Gilver &> /dev/null
 	zip -r9 "../$ZIPNAME" * -x .git modules\* patch\* ramdisk\* README.md *placeholder
 	cd ..
 	rm -rf AnyKernel3
+	rm -rf out
 	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 	echo "Zip: $ZIPNAME"
 else
