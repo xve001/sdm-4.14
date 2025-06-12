amackpro/android_kernel_xiaomi_sm8250

drivers: import oplus oplus_wakelock && oplus_subsys_sleep_monitor

kernel/power/suspend.c
@@ -332,9 +332,18 @@ MODULE_PARM_DESC(pm_test_delay,
 static int suspend_test(int level)
 {
 #ifdef CONFIG_PM_DEBUG
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s pm_test_level:%d, level:%d\n", __func__,
+                pm_test_level, level);
 	if (pm_test_level == level) {
+#endif
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
 		pr_info("suspend debug: Waiting for %d second(s).\n",
 				pm_test_delay);
+#else
+		pr_err("suspend debug: Waiting for %d second(s).\n",
+                                pm_test_delay);
+#endif
 		mdelay(pm_test_delay * 1000);
 		return 1;
 	}
@@ -403,8 +412,15 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	int error, last_dev;
 
 	error = platform_suspend_prepare(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Platform_finish;
+#else
+	if (error) {
+		pr_info("%s platform_suspend_prepare fail\n", __func__);
+		goto Platform_finish;
+	}
+#endif
 
 	error = dpm_suspend_late(PMSG_SUSPEND);
 	if (error) {
@@ -416,8 +432,16 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_finish;
 	}
 	error = platform_suspend_prepare_late(state);
+
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Devices_early_resume;
+#else
+	if (error) {
+		pr_info("%s prepare late fail\n", __func__);
+		goto Devices_early_resume;
+	}
+#endif
 
 	if (state == PM_SUSPEND_TO_IDLE && pm_test_level != TEST_PLATFORM) {
 		s2idle_loop();
@@ -434,11 +458,24 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_early_resume;
 	}
 	error = platform_suspend_prepare_noirq(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Platform_wake;
-
+#else
+	if (error) {
+		pr_info("%s prepare_noirq fail\n", __func__);
+		goto Platform_wake;
+	}
+#endif
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (suspend_test(TEST_PLATFORM))
 		goto Platform_wake;
+#else
+	if (suspend_test(TEST_PLATFORM)) {
+		pr_info("%s test_platform fail\n", __func__);
+		goto Platform_wake;
+	}
+#endif
 
 	error = disable_nonboot_cpus();
 	if (error || suspend_test(TEST_CPUS)) {
@@ -447,6 +484,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	}
 
 	arch_suspend_disable_irqs();
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s syscore_suspend\n", __func__);
+#endif
 	BUG_ON(!irqs_disabled());
 
 	system_state = SYSTEM_SUSPEND;
@@ -498,15 +538,28 @@ int suspend_devices_and_enter(suspend_state_t state)
 	int error;
 	bool wakeup = false;
 
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (!sleep_state_supported(state))
 		return -ENOSYS;
+#else
+	if (!sleep_state_supported(state)) {
+		pr_info("sleep_state_supported false\n");
+		return -ENOSYS;
+	}
+#endif
 
 	pm_suspend_target_state = state;
 
 	error = platform_suspend_begin(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Close;
-
+#else
+	if (error) {
+		pr_info("%s platform_suspend_begin fail\n", __func__);
+		goto Close;
+	}
+#endif
 	suspend_console();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
@@ -517,12 +570,22 @@ int suspend_devices_and_enter(suspend_state_t state)
 		goto Recover_platform;
 	}
 	suspend_test_finish("suspend devices");
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (suspend_test(TEST_DEVICES))
 		goto Recover_platform;
+#else
+	if (suspend_test(TEST_DEVICES)) {
+		pr_info("%s TEST_DEVICES fail\n", __func__);
+		goto Recover_platform;
+	}
+#endif
 
 	do {
 		error = suspend_enter(state, &wakeup);
 	} while (!error && !wakeup && platform_suspend_again(state));
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("suspend_enter end, error:%d, wakeup:%d\n", error, wakeup);
+#endif
 
  Resume_devices:
 	suspend_test_start();
@@ -556,6 +619,65 @@ static void suspend_finish(void)
 }
 
 /**
+* Sync the filesystem in seperate workqueue.
+* Then check it finishing or not periodically and
+* abort if any wakeup source comes in. That can reduce
+* the wakeup latency
+*
+*/
+static bool sys_sync_completed = false;
+static void sys_sync_work_func(struct work_struct *work);
+static DECLARE_WORK(sys_sync_work, sys_sync_work_func);
+static DECLARE_WAIT_QUEUE_HEAD(sys_sync_wait);
+static void sys_sync_work_func(struct work_struct *work)
+{
+    trace_suspend_resume(TPS("sync_filesystems"), 0, true);
+    pr_info(KERN_INFO "PM: Syncing filesystems ... ");
+    ksys_sync();
+    pr_cont("done.\n");
+    trace_suspend_resume(TPS("sync_filesystems"), 0, false);
+    sys_sync_completed = true;
+    wake_up(&sys_sync_wait);
+}
+
+static int sys_sync_queue(void)
+{
+    int work_status = work_busy(&sys_sync_work);
+
+    /*maybe some irq coming here before pending check*/
+    pm_wakeup_clear(true);
+
+    /*Check if the previous work still running.*/
+    if (!(work_status & WORK_BUSY_PENDING)) {
+        if (work_status & WORK_BUSY_RUNNING) {
+            while (wait_event_timeout(sys_sync_wait, sys_sync_completed,
+                        msecs_to_jiffies(100)) == 0) {
+                if (pm_wakeup_pending()) {
+                    pr_info("PM: Pre-Syncing abort\n");
+                    goto abort;
+                }
+            }
+            pr_info("PM: Pre-Syncing done\n");
+        }
+        sys_sync_completed = false;
+        schedule_work(&sys_sync_work);
+    }
+
+    while (wait_event_timeout(sys_sync_wait, sys_sync_completed,
+                    msecs_to_jiffies(100)) == 0) {
+        if (pm_wakeup_pending()) {
+            pr_info("PM: Syncing abort\n");
+            goto abort;
+        }
+    }
+
+    pr_info("PM: Syncing done\n");
+    return 0;
+abort:
+    return -EAGAIN;
+}
+
+/**
  * enter_state - Do common work needed to enter system sleep state.
  * @state: System sleep state to enter.
  *
@@ -576,10 +698,20 @@ static int enter_state(suspend_state_t state)
 		}
 #endif
 	} else if (!valid_state(state)) {
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+		pr_info("%s invalid_state\n", __func__);
+#endif
 		return -EINVAL;
 	}
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (!mutex_trylock(&system_transition_mutex))
 		return -EBUSY;
+#else
+	if (!mutex_trylock(&system_transition_mutex)) {
+		pr_info("%s mutex_trylock fail\n", __func__);
+		return -EBUSY;
+	}
+#endif
 
 	if (state == PM_SUSPEND_TO_IDLE)
 		s2idle_begin();
@@ -595,8 +727,17 @@ static int enter_state(suspend_state_t state)
 	pm_pr_dbg("Preparing system for sleep (%s)\n", mem_sleep_labels[state]);
 	pm_suspend_clear_flags();
 	error = suspend_prepare(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Unlock;
+
+	pr_info("%s suspend_prepare success\n", __func__);
+#else
+	if (error) {
+		pr_info("%s suspend_prepare error:%d\n", __func__, error);
+		goto Unlock;
+	}
+#endif
 
 	if (suspend_test(TEST_FREEZER))
 		goto Finish;
@@ -606,6 +747,9 @@ static int enter_state(suspend_state_t state)
 	pm_restrict_gfp_mask();
 	error = suspend_devices_and_enter(state);
 	pm_restore_gfp_mask();
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s suspend_devices_and_enter end\n", __func__);
+#endif
 
  Finish:
 	events_check_enabled = false;

xve001/sdm-4.14

	modified:   drivers/base/power/wakeup.c
	modified:   drivers/irqchip/irq-gic-v3.c
	modified:   drivers/oplus/Kconfig
	modified:   drivers/oplus/Makefile
	new file:   drivers/oplus/oplus_subsys_sleep_monitor.c
	new file:   drivers/oplus/oplus_wakelock_profiler.c
	new file:   drivers/soc/qcom/qcom_ipcc.c
	modified:   include/linux/pm_wakeup.h
	modified:   include/net/netns/ipv4.h
	new file:   include/net/oplus_nwpower.h
	modified:   include/net/sock.h
	modified:   include/net/tcp.h
	new file:   include/oplus/oplus_subsys_sleep_monitor.h
	new file:   include/oplus/oplus_wakelock_profiler.h
	new file:   include/oplus/qcom_platform_kona.h
	modified:   include/uapi/linux/netlink.h
	modified:   kernel/power/main.c
	modified:   kernel/power/suspend.c
	modified:   kernel/power/wakeup_reason.c
	modified:   kernel/time/alarmtimer.c
	modified:   net/Makefile
	modified:   net/ipv4/sysctl_net_ipv4.c
	modified:   net/ipv4/tcp.c
	modified:   net/ipv4/tcp_input.c
	modified:   net/ipv4/tcp_ipv4.c
	modified:   net/ipv4/tcp_output.c
	modified:   net/ipv4/tcp_timer.c
	modified:   net/ipv6/ip6_output.c
	modified:   net/ipv6/route.c
	modified:   net/ipv6/tcp_ipv6.c
	modified:   net/netfilter/xt_HARDIDLETIMER.c
	new file:   net/oplus_ipv6/Makefile
	new file:   net/oplus_ipv6/oplus_ipv6_rto.c
	new file:   net/oplus_nwpower/Makefile
	new file:   net/oplus_nwpower/oplus_nwpower.c
	modified:   net/socket.c

kernel/power/suspend.c
@@ -332,9 +332,18 @@ MODULE_PARM_DESC(pm_test_delay,
 static int suspend_test(int level)
 {
 #ifdef CONFIG_PM_DEBUG
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s pm_test_level:%d, level:%d\n", __func__,
+                pm_test_level, level);
 	if (pm_test_level == level) {
+#endif
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
 		pr_debug("suspend debug: Waiting for %d second(s).\n",
 				pm_test_delay);
+#else
+		pr_err("suspend debug: Waiting for %d second(s).\n",
+                                pm_test_delay);
+#endif
 		mdelay(pm_test_delay * 1000);
 		return 1;
 	}
@@ -403,8 +412,15 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	int error, last_dev;
 
 	error = platform_suspend_prepare(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Platform_finish;
+#else
+	if (error) {
+		pr_info("%s platform_suspend_prepare fail\n", __func__);
+		goto Platform_finish;
+	}
+#endif
 
 	error = dpm_suspend_late(PMSG_SUSPEND);
 	if (error) {
@@ -416,8 +432,16 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_finish;
 	}
 	error = platform_suspend_prepare_late(state);
+
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Devices_early_resume;
+#else
+	if (error) {
+		pr_info("%s prepare late fail\n", __func__);
+		goto Devices_early_resume;
+	}
+#endif
 
 	if (state == PM_SUSPEND_TO_IDLE && pm_test_level != TEST_PLATFORM) {
 		s2idle_loop();
@@ -434,11 +458,24 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_early_resume;
 	}
 	error = platform_suspend_prepare_noirq(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Platform_wake;
-
+#else
+	if (error) {
+		pr_info("%s prepare_noirq fail\n", __func__);
+		goto Platform_wake;
+	}
+#endif
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (suspend_test(TEST_PLATFORM))
 		goto Platform_wake;
+#else
+	if (suspend_test(TEST_PLATFORM)) {
+		pr_info("%s test_platform fail\n", __func__);
+		goto Platform_wake;
+	}
+#endif
 
 	error = disable_nonboot_cpus();
 	if (error || suspend_test(TEST_CPUS)) {
@@ -447,6 +484,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	}
 
 	arch_suspend_disable_irqs();
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s syscore_suspend\n", __func__);
+#endif
 	BUG_ON(!irqs_disabled());
 
 	system_state = SYSTEM_SUSPEND;
@@ -501,15 +541,28 @@ int suspend_devices_and_enter(suspend_state_t state)
 	int error;
 	bool wakeup = false;
 
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (!sleep_state_supported(state))
 		return -ENOSYS;
+#else
+	if (!sleep_state_supported(state)) {
+		pr_info("sleep_state_supported false\n");
+		return -ENOSYS;
+	}
+#endif
 
 	pm_suspend_target_state = state;
 
 	error = platform_suspend_begin(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Close;
-
+#else
+	if (error) {
+		pr_info("%s platform_suspend_begin fail\n", __func__);
+		goto Close;
+	}
+#endif
 	suspend_console();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
@@ -520,12 +573,22 @@ int suspend_devices_and_enter(suspend_state_t state)
 		goto Recover_platform;
 	}
 	suspend_test_finish("suspend devices");
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (suspend_test(TEST_DEVICES))
 		goto Recover_platform;
+#else
+	if (suspend_test(TEST_DEVICES)) {
+		pr_info("%s TEST_DEVICES fail\n", __func__);
+		goto Recover_platform;
+	}
+#endif
 
 	do {
 		error = suspend_enter(state, &wakeup);
 	} while (!error && !wakeup && platform_suspend_again(state));
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("suspend_enter end, error:%d, wakeup:%d\n", error, wakeup);
+#endif
 
  Resume_devices:
 	suspend_test_start();
@@ -559,6 +622,65 @@ static void suspend_finish(void)
 }
 
 /**
+* Sync the filesystem in seperate workqueue.
+* Then check it finishing or not periodically and
+* abort if any wakeup source comes in. That can reduce
+* the wakeup latency
+*
+*/
+static bool sys_sync_completed = false;
+static void sys_sync_work_func(struct work_struct *work);
+static DECLARE_WORK(sys_sync_work, sys_sync_work_func);
+static DECLARE_WAIT_QUEUE_HEAD(sys_sync_wait);
+static void sys_sync_work_func(struct work_struct *work)
+{
+    trace_suspend_resume(TPS("sync_filesystems"), 0, true);
+    pr_info(KERN_INFO "PM: Syncing filesystems ... ");
+    ksys_sync();
+    pr_cont("done.\n");
+    trace_suspend_resume(TPS("sync_filesystems"), 0, false);
+    sys_sync_completed = true;
+    wake_up(&sys_sync_wait);
+}
+
+static int sys_sync_queue(void)
+{
+    int work_status = work_busy(&sys_sync_work);
+
+    /*maybe some irq coming here before pending check*/
+    pm_wakeup_clear(true);
+
+    /*Check if the previous work still running.*/
+    if (!(work_status & WORK_BUSY_PENDING)) {
+        if (work_status & WORK_BUSY_RUNNING) {
+            while (wait_event_timeout(sys_sync_wait, sys_sync_completed,
+                        msecs_to_jiffies(100)) == 0) {
+                if (pm_wakeup_pending()) {
+                    pr_info("PM: Pre-Syncing abort\n");
+                    goto abort;
+                }
+            }
+            pr_info("PM: Pre-Syncing done\n");
+        }
+        sys_sync_completed = false;
+        schedule_work(&sys_sync_work);
+    }
+
+    while (wait_event_timeout(sys_sync_wait, sys_sync_completed,
+                    msecs_to_jiffies(100)) == 0) {
+        if (pm_wakeup_pending()) {
+            pr_info("PM: Syncing abort\n");
+            goto abort;
+        }
+    }
+
+    pr_info("PM: Syncing done\n");
+    return 0;
+abort:
+    return -EAGAIN;
+}
+
+/**
  * enter_state - Do common work needed to enter system sleep state.
  * @state: System sleep state to enter.
  *
@@ -579,10 +701,20 @@ static int enter_state(suspend_state_t state)
 		}
 #endif
 	} else if (!valid_state(state)) {
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+		pr_info("%s invalid_state\n", __func__);
+#endif
 		return -EINVAL;
 	}
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (!mutex_trylock(&pm_mutex))
 		return -EBUSY;
+#else
+	if (!mutex_trylock(&pm_mutex)) {
+		pr_info("%s mutex_trylock fail\n", __func__);
+		return -EBUSY;
+	}
+#endif
 
 	pm_wakeup_clear(true);
 	if (state == PM_SUSPEND_TO_IDLE)
@@ -599,8 +731,17 @@ static int enter_state(suspend_state_t state)
 	pm_pr_dbg("Preparing system for sleep (%s)\n", mem_sleep_labels[state]);
 	pm_suspend_clear_flags();
 	error = suspend_prepare(state);
+#ifndef CONFIG_OPLUS_WAKELOCK_PROFILER
 	if (error)
 		goto Unlock;
+
+	pr_info("%s suspend_prepare success\n", __func__);
+#else
+	if (error) {
+		pr_info("%s suspend_prepare error:%d\n", __func__, error);
+		goto Unlock;
+	}
+#endif
 
 	if (suspend_test(TEST_FREEZER))
 		goto Finish;
@@ -610,6 +751,9 @@ static int enter_state(suspend_state_t state)
 	pm_restrict_gfp_mask();
 	error = suspend_devices_and_enter(state);
 	pm_restore_gfp_mask();
+#ifdef CONFIG_OPLUS_WAKELOCK_PROFILER
+	pr_info("%s suspend_devices_and_enter end\n", __func__);
+#endif
 
  Finish:
 	events_check_enabled = false;