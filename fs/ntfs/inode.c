ecea3a201ab07c3d95df7d74dd4831b50a52c56a
xve001 <nandaanamee@gmail.com>
2025-06-12 16:09:58 +0000

    ../fs/ntfs/inode.c:568:16: error: assigning to 'atomic64_t' from incompatible type 'int'   568 |         vi->i_version = 1;       |                       ^ ~ 1 error generated.

--- fs/ntfs/inode.c
+++ fs/ntfs/inode.c
@@ -565,7 +565,7 @@ static int ntfs_read_locked_inode(struct inode *vi)
 	 * This is for checking whether an inode has changed w.r.t. a file so
 	 * that the file can be updated if necessary (compare with f_version).
 	 */
-	vi->i_version = 1;
+	atomic64_set(&vi->i_version, 1);
 
 	vi->i_uid = vol->uid;
 	vi->i_gid = vol->gid;
